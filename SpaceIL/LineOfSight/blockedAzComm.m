437f8e0620a4d4389fb605c721df6175ecd6e7e9
Yonatan <50169834+yonatansmn@users.noreply.github.com>
2021-01-10 15:25:19 +0200

    Update blockedAzComm.m

--- SpaceIL/LineOfSight/blockedAzComm.m
+++ SpaceIL/LineOfSight/blockedAzComm.m
@@ -58,11 +58,7 @@ azbw=1;
 azbins=-180:azbw:180;
 f=find(d<r*1e3);
 zp=zeros(size(zz));
-% c=1;
-% maxi=1;
-% m=zeros(1,1); k=zeros(length(f),0);
 
-profile on
 for i=1:length(f)
     xxr=xx-xx(f(i)); yyr=yy-yy(f(i));
     azr=atan2d(yyr,xxr);  
@@ -72,20 +68,11 @@ for i=1:length(f)
     fd=find(dr>3*res*1e3);
     idx=find(slr(fd)>ea);
     if idx>0
-%         k(c)=i;      
-%         if (length(idx)>maxi)
-%              m(1:c-1,maxi+1:length(idx))=0;
-%              maxi=length(idx);
-%         end
-%         m(c,:)=padarray(idx,maxi-length(idx),'post');
-%         c=c+1;
         obsaz=unique(round(azr(idx))); % in what azimuths relative to the point are obstacles
         zp(f(i))=length(obsaz)/length(azbins);
     end
 end
-% profile viewer
 
-% k(k==0)=[];
 
 % setting map to ellipse size
 clear xl xf yl yf
@@ -109,54 +96,3 @@ title 'Rate';
 xlabel 'km'; ylabel 'km';
 set(gca,'xtick',-r:3:r);
 set(gca,'ytick',-r:3:r);
-
-% sortzr=sort(zr(:));
-% fin=find(isnan(sortzr),1);
-% sortzr=sortzr(1:(fin-1));
-% pos2 = [0.6 0.2 0.3 0.58];
-% subplot('Position',pos2)
-% plot(sortzr,(1:length(sortzr))/length(sortzr)); 
-% grid on
-% xlabel 'Blocked Az/Total Az'; ylabel 'CDF';
-% title 'Cumulative Distribution Function';
-% axis tight
-
-%convert saved data to azimuth, distance,center point coordinates & obstacle coordinates matrices
-%example: for center point [xc(1),yc(1)] there are obstacles in points
-%[xobs(1,:),yobs(1,:)]. those obstacles are in azimuths azobs(1,:) and
-%distance (in metres) dobs(1,:).
-
-% azobs=zeros(size(m)); dobs=zeros(size(m));
-% xc=zeros(size(m,1))'; yc=zeros(size(m,1))';
-% xobs=zeros(size(m)); yobs=zeros(size(m));
-% col=length(m(1,:)); row=length(m(:,1));
-% 
-% if k>0
-% for i=1:row
-%     xc(i)=xx(f(k(i))); yc(i)=yy(f(k(i)));
-%     for j=1:col
-%         if m(i,j)>0
-%             xobs(i,j)=xx(m(i,j));
-%             yobs(i,j)=yy(m(i,j));
-%             delx=xobs(i,j)-xc(i);
-%             dely=yobs(i,j)-yc(i);
-%             dobs(i,j)=sqrt(delx^2+dely^2)*1e3;
-%             azobs(i,j)=atan2d(dely,delx);
-%         else
-%             break;
-%         end
-%     end
-% end
-% end
-% 
-% azobs=azobsc; dobs=dobsc; xc=xcc; yc=ycc; xobs=xobsc; yobs=yobsc; zr=zrc; xr=xrc; yr=yrc;
-% fn='dataComm2';
-% 
-% switch site
-%     case 'Posidonius'
-% save([pwd,'\Line_of_sight\posidonius 9.4x11\'...
-%     ,fn],'azobs','dobs','xc','yc','xobs','yobs','zr','yr','xr');       
-%     case 'Wohler'
-% save([pwd,'\Line_of_sight\wohler big 7x8.9\'...
-%     ,fn],'azobs','dobs','xc','yc','xobs','yobs','zr','yr','xr');       
-% end
